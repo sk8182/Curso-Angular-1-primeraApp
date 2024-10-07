511d396d0b12631ea3f668eeb2de2dc3bd47c9b1
Julio Martinez <sk8182enkidu@gmail.com>
2024-10-07 11:57:07 +0200

    Angular. Interpolación. Vídeo 7 - cómo funciona la interpolación de String en Angular

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -5,11 +5,19 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-empleado',
   standalone: true,
   imports: [RouterOutlet],
-  //templateUrl: './empleado.component.html',
-  template:"<p>aquí iría un empleado</p>",
-  //styleUrl: './empleado.component.css'
-  styles:["p{background-color: blueviolet;}"]//le meto el css o html directamente
+  templateUrl: './empleado.component.html',
+  styleUrl: './empleado.component.css'
+ 
 })
 export class EmpleadoComponent {
+
+  nombre= "Juan";
+  apellido="Díaz";
+  private edad=8;//si modifico y pongo un private necesitaré un getter para acceder
+  empresa="Píldoras Informaticas";
+
+  getEdad(){
+    return this.edad;
+  }
 
 }
